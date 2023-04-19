98e67c611c1509131be5d6c629a641cf949b17c3
Sentry2703 <adetunjiadewoye1@gmail.com>
2023-04-18 21:50:17 -0400

    Called the Quiz Questions

---
+++ app/src/main/java/com/example/quizzle/Question.kt
@@ -0,0 +1,8 @@
+package com.example.quizzle
+
+class Question {
+    var question: String? = null
+    var correctAnswer: String? = null
+    var wrongAnswers: List<String>? = null
+
+}
---
+++ app/src/main/java/com/example/quizzle/QuizActivity.kt
@@ -0,0 +1,13 @@
+package com.example.quizzle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class QuizActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+    }
+}
